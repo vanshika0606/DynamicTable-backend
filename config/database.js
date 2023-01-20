66bc55477d226d65bbb52f0d04698bfa5ef30e81
Vanshika <vanshika06065@gmail.com>
2023-01-20 22:23:32 +0530

    string added

--- config/database.js
+++ config/database.js
@@ -4,7 +4,7 @@ const mongoose = require("mongoose");
 const connectDatabase = ()=>{
 
 
-    mongoose.connect("mongodb://localhost:27017/spotlight" , {useNewUrlParser:true}).then((data)=>{
+    mongoose.connect("mongodb+srv://vanshika:Vanshika66@mern.4iuapb3.mongodb.net/?retryWrites=true&w=majority" , {useNewUrlParser:true}).then((data)=>{
     console.log(`Mongodb connect with server 4000`)
 }).catch(()=>{
     console.log(err)
