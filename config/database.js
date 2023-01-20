c8b35760dd6ed86b157ad1c50df4f99d10dc107b
Vanshika <vanshika06065@gmail.com>
2023-01-20 22:55:42 +0530

    done

--- config/database.js
+++ config/database.js
@@ -6,7 +6,7 @@ const connectDatabase = ()=>{
 
     mongoose.connect("mongodb+srv://vanshika:Vanshika66@mern.4iuapb3.mongodb.net/?retryWrites=true&w=majority" , {useNewUrlParser:true}).then((data)=>{
     console.log(`Mongodb connect with server 4000`)
-}).catch(()=>{
+}).catch((err)=>{
     console.log(err)
 })
 
